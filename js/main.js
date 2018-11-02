112fc1b7d401a05d631d354ba5e0e4a092f57d9e
Christian D <christian.diemers@gmail.com>
2018-11-03 00:02:51 +0100

    Fixed Scrolling

--- js/main.js
+++ js/main.js
@@ -6,10 +6,13 @@ $(document).ready(function () {
 		easing: "ease",                  // Easing options accepts the CSS3 easing animation such "ease", "linear", "ease-in",
 		// "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
 		animationTime: 800,             // AnimationTime let you define how long each section takes to animate
-		pagination: true,                // You can either show or hide the pagination. Toggle true for show, false for hide.
+		pagination: false,                // You can either show or hide the pagination. Toggle true for show, false for hide.
 		updateURL: false,                // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
-		beforeMove: function (index) { },  // This option accepts a callback function. The function will be called before the page moves.
-		afterMove: function (index) { },   // This option accepts a callback function. The function will be called after the page moves.
+		beforeMove: function (index) {
+			markAsideDot(index);
+		},
+		afterMove: function (index) {
+		},
 		loop: false,                     // You can have the page loop back to the top/bottom when the user navigates at up/down on the first/last page.
 		keyboard: true,                  // You can activate the keyboard controls
 		responsiveFallback: false,        // You can fallback to normal page scroll by defining the width of the browser in which
@@ -178,4 +181,17 @@ $(document).ready(function () {
 		}
 	}
 
+	// indicator click listener
+	$.map($('.dot'), (val, i) => {
+		$(val).click(() => {
+			$('.main').moveTo(i + 1);
+		});
+	});
+
+	// set correct scroll indicator color
+	function markAsideDot(index) {
+		console.log(index);
+		$('.dot').css("background-color", "var(--light)");
+		$('.dot:eq(' + --index + ')').css("background-color", "var(--primary2)");
+	}
 });
